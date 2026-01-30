eda0d77b2426f79e85987897910a3f3187397762
Gtwo <131792434+MitolGtwo@users.noreply.github.com>
2026-01-29 19:26:48 -0500

    Create DeathKorpsOperatives.kt

---
+++ app/src/main/java/com/example/killteamruleset/ui/data/Empire/list/DeathKorps/DeathKorpsOperatives.kt
@@ -0,0 +1,20 @@
+package com.example.killteamruleset.ui.data.Empire.list.DeathKorps
+
+/*import com.example.killteamruleset.ui.data.Empire.list.SkitariiRangerAlpha
+*/
+import com.example.killteamruleset.ui.model.Operative
+
+val HunterCladeOperatives = listOf(
+    DeathKorpsWatchmaster,
+    DeathKorpsBruiser,
+    DeathKorpsConfidant,
+    DeathKorpsGunner,
+    DeathKorpsMedic,
+    DeathKorpsSapper,
+    DeathKorpsSniper,
+    DeathKorpsSpotter,
+    DeathKorpsTrooper,
+    DeathKorpsVeteran,
+    DeathKorpsVoxOperator,
+    DeathKorpsZealot
+)
