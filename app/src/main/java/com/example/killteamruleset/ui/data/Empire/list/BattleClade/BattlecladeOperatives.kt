4a890b912c8717d3609be7e6ea45fe556c1bb9e8
Gtwo <131792434+MitolGtwo@users.noreply.github.com>
2026-01-29 19:38:59 -0500

    Create BattlecladeOperatives.kt

---
+++ app/src/main/java/com/example/killteamruleset/ui/data/Empire/list/BattleClade/BattlecladeOperatives.kt
@@ -0,0 +1,14 @@
+package com.example.killteamruleset.ui.data.Empire.list.BattleClade
+
+import com.example.killteamruleset.ui.data.Empire.list.SkitariiRangerAlpha
+import com.example.killteamruleset.ui.model.Operative
+
+val HunterCladeOperatives = listOf(
+  BattleCladeTechnoarcheologist,
+  BattleCladeServitorUnderseer,
+  BattleCladeAutoProxyServitor,
+  BattleCladeBreacherServitor,
+  BattleCladeCombatServitor,
+  BattleCladeGunServitor,
+  BattleCladeTechnomedicServitor
+)
