3fccd695b9ae2f43c8feced4506703f7bedbd62c
Gtwo <131792434+MitolGtwo@users.noreply.github.com>
2026-02-19 18:31:59 -0500

    Create DeathWatchOperativeSelection.kt

---
+++ app/src/main/java/com/example/killteamruleset/ui/data/Empire/list/DeathWatch/DeathWatchOperativeSelection.kt
@@ -0,0 +1,61 @@
+package com.example.killteamruleset.ui.data.Empire.list.DeathWatch
+
+import com.example.killteamruleset.ui.model.OperativeSelectionRule
+
+val DeathWatchOperativeSelection: List<OperativeSelectionRule> = listOf(
+
+    OperativeSelectionRule(
+        text = "5 DEATHWATCH Operatives selected from the following list:"
+    ),
+
+    OperativeSelectionRule(
+        text = "WATCH SERGEANT"
+        indent = 1
+    ),
+      OperativeSelectionRule(
+        text = "AEGIS"
+        indent = 1
+    ),
+      OperativeSelectionRule(
+        text = "BLADEMASTER"
+        indent = 1
+    ),
+      OperativeSelectionRule(
+        text = "BOMBARD"
+        indent = 1
+    ),
+      OperativeSelectionRule(
+        text = "BREACHER"
+        indent = 1
+    ),
+      OperativeSelectionRule(
+        text = "DEMOLISHER"
+        indent = 1
+    ),
+      OperativeSelectionRule(
+        text = "DISRUPTOR"
+        indent = 1
+    ),
+      OperativeSelectionRule(
+        text = "GUNNER"
+        indent = 1
+    ),
+      OperativeSelectionRule(
+        text = "HEADTAKER"
+        indent = 1
+    ),
+      OperativeSelectionRule(
+        text = "HORDE-SLAYER"
+        indent = 1
+    ),
+      OperativeSelectionRule(
+        text = "MARKSMAN"
+        indent = 1
+    ),
+     
+
+    OperativeSelectionRule(
+        text = "Your kill team can only include each Operative on this list once, and can only include up to one GRAVIS Operative.",
+        isFooter = true
+    )
+)
