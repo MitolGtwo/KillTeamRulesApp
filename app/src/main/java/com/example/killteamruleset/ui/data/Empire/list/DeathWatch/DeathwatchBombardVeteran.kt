ccd1b15e464563c0e3a2cabef67e8ab442724473
Gtwo <131792434+MitolGtwo@users.noreply.github.com>
2026-02-19 18:01:52 -0500

    Update DeathwatchBombardVeteran.kt

--- app/src/main/java/com/example/killteamruleset/ui/data/Empire/list/DeathWatch/DeathwatchBombardVeteran.kt
+++ app/src/main/java/com/example/killteamruleset/ui/data/Empire/list/DeathWatch/DeathwatchBombardVeteran.kt
@@ -1,1 +1,71 @@
 package com.example.killteamruleset.ui.data.Empire.list.DeathWatch
+
+import com.example.killteamruleset.R
+import com.example.killteamruleset.ui.model.*
+
+
+val DeathwatchBombardVeteran = Operative(
+    name = "Deathwatch Bombard Veteran",
+    imageRes = R.drawable.alpharanger,
+    stats = OperativeStats(
+        apl = 3,
+        move = "5\"",
+        save = "3+",
+        wounds = 18
+    ),
+    weapons = listOf(
+        WeaponProfile(
+            name = "Bolt Pistol",
+            type = WeaponType.RANGED,
+            attacks = 4,
+            hit = "3+",
+            damage = "3/4",
+            keywords = listOf(
+               Range(8)
+            )
+        ),
+              WeaponProfile(
+            name = "Frag Cannon (shell)",
+            type = WeaponType.RANGED,
+            attacks = 4,
+            hit = "3+",
+            damage = "5/7",
+            keywords = listOf(
+              Piercing(1)
+            )
+        ),
+              WeaponProfile(
+            name = "Frag Cannon (shrapnel)",
+            type = WeaponType.RANGED,
+            attacks = 5,
+            hit = "3+",
+            damage = "4/5",
+            keywords = listOf(
+              Torrent(2)
+            )
+        ),
+              WeaponProfile(
+            name = "Fists",
+            type = WeaponType.RANGED,
+            attacks = 4,
+            hit = "3+",
+            damage = "3/4",
+            keywords = listOf(
+            )
+    ),
+
+    abilities = listOf(
+       
+    ),
+
+
+    keywords = listOf(
+        "DEATHWATCH",
+        "IMPERIUM",
+        "ADEPTUS ASTARTES",
+        "GRAVIS",
+        "BOMBARD",
+        "32MM"
+
+    )
+)
