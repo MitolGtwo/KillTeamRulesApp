e4902f9c9d1b17066aa9c4da272b3bbdeb368016
Gtwo <131792434+MitolGtwo@users.noreply.github.com>
2026-02-19 14:43:14 -0500

    Create CelestianOperativeSelection.kt

---
+++ app/src/main/java/com/example/killteamruleset/ui/data/Empire/list/CelestianInsidiants/CelestianOperativeSelection.kt
@@ -0,0 +1,14 @@
+package com.example.killteamruleset.ui.data.Empire.list.CelestianInsidiants
+
+import com.example.killteamruleset.ui.model.Operative
+
+val CelestianInsidiantOperatives = listOf(
+  InsidiantSuperior,
+  InsidiantAbjuror,
+  InsidiantCensor,
+  InsidiantCremator,
+  InsidiantDenuncia,
+  InsidiantMortisanctus,
+  InsidiantReliquarius,
+  InsidiantWarrior
+)
