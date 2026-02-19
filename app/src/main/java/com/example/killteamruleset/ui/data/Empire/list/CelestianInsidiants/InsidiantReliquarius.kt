1f5ce0a219da3e0409b64e8dd4626c26cc91ec60
Gtwo <131792434+MitolGtwo@users.noreply.github.com>
2026-02-19 14:04:55 -0500

    Update InsidiantReliquarius.kt

--- app/src/main/java/com/example/killteamruleset/ui/data/Empire/list/CelestianInsidiants/InsidiantReliquarius.kt
+++ app/src/main/java/com/example/killteamruleset/ui/data/Empire/list/CelestianInsidiants/InsidiantReliquarius.kt
@@ -1,2 +1,84 @@
 package com.example.killteamruleset.ui.data.Empire.list.CelestianInsidiants
 
+import com.example.killteamruleset.R
+import com.example.killteamruleset.ui.model.Ability
+import com.example.killteamruleset.ui.model.Brutal
+import com.example.killteamruleset.ui.model.Devastating
+import com.example.killteamruleset.ui.model.Lethal
+import com.example.killteamruleset.ui.model.Operative
+import com.example.killteamruleset.ui.model.OperativeStats
+import com.example.killteamruleset.ui.model.Range
+import com.example.killteamruleset.ui.model.Severe
+import com.example.killteamruleset.ui.model.Shock
+import com.example.killteamruleset.ui.model.WeaponProfile
+import com.example.killteamruleset.ui.model.WeaponType
+
+val InsidiatReliquarius: Operative
+    get() = Operative(
+        name = "Insidiat Reliquarius",
+        imageRes = R.drawable.alpharanger,
+        stats = OperativeStats(
+            apl = 2,
+            move = "6\"",
+            save = "3+",
+            wounds = 9
+        ),
+        weapons = listOf(
+           WeaponProfile(
+                name = "Bolt Pistol",
+                type = WeaponType.RANGED,
+                attacks = 4,
+                hit = "3+",
+                damage = "3/4",
+                keywords = listOf(
+                    Range(8)
+                )
+        ),
+           WeaponProfile(
+                name = "Condemnor Stakethrower",
+                type = WeaponType.RANGED,
+                attacks = 4,
+                hit = "3+",
+                damage = "2/2",
+                keywords = listOf(
+                  Devastating(1),
+                  Piercing_Crits(1),
+                  Silent
+                ),
+                
+                )
+        ),
+           WeaponProfile(
+                name = "Gun butt",
+                type = WeaponType.MELEE,
+                attacks =3,
+                hit = "3+",
+                damage = "2/3",
+                keywords = listOf(
+                    Lethal(5),
+                    Brutal
+                )
+        ),
+
+        abilities = listOf(
+            Ability(
+                title = "Zealous Ultimatum",
+                usage = R.string.battleclade_divinearcana_usage,
+                description = R.string.battleclade_divinearcana_description
+            ),
+
+            Ability(
+                title = "Bladed Stance",
+                usage = R.string.battleclade_omniscanner_usage,
+                description = R.string.battleclade_omniscanner_description
+            )
+        ),
+
+        keywords = listOf(
+            "CELESTIAN INSIDIANT",
+            "IMPERIUM",
+            "ADEPTA SORORITAS",
+            "RELIQUARIUS",
+            "32MM"
+        )
+    )
