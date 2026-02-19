8591c21ebe0538274fcbd0cd6e85cecfa616fb17
Gtwo <131792434+MitolGtwo@users.noreply.github.com>
2026-02-19 18:28:01 -0500

    Create DeathwatchOperatives.kt

---
+++ app/src/main/java/com/example/killteamruleset/ui/data/Empire/list/DeathWatch/DeathwatchOperatives.kt
@@ -0,0 +1,18 @@
+package com.example.killteamruleset.ui.data.Empire.list.DeathWatch
+
+
+import com.example.killteamruleset.ui.model.Operative
+
+val DeathWatchOperatives = listOf(
+  DeathwatchWatchSergeant,
+  DeathwatchAegisVeteran,
+  DeathwatchBlademasterVeteran,
+  DeathwatchBombardVeteran,
+  DeathwatchBreacherVeteran,
+  DeathwatchDemolisherVeteran,
+  DeathwatchDisruptorVeteran,
+  DeathwatchGunnerVeteran,
+  DeathwatchHeadtakerVeteran,
+  DeathwatchHordeslayerVeteran,
+  DeathwatchMarksmanVeteran
+)
