0a0a5689a485215e4d24ead6a381035625f57415
Gtwo <131792434+MitolGtwo@users.noreply.github.com>
2026-02-19 19:37:13 -0500

    Create ElucidianOperativeSelection.kt

---
+++ app/src/main/java/com/example/killteamruleset/ui/data/Empire/list/ElucidianStarstriders/ElucidianOperativeSelection.kt
@@ -0,0 +1,44 @@
+package com.example.killteamruleset.ui.data.Empire.list.DeathKorps
+
+import com.example.killteamruleset.ui.model.OperativeSelectionRule
+
+val ElucidianOperativeSelection: List<OperativeSelectionRule> = listOf(
+
+    OperativeSelectionRule(
+        text = "Every ELUCIDIAN STARSTRIDER Operative in the following list:"
+    ),
+
+    OperativeSelectionRule(
+        text = "1 ELUCIA VHANE"
+        indent = 1
+    ),
+      OperativeSelectionRule(
+        text = "1 CANID"
+        indent = 1
+    ),
+      OperativeSelectionRule(
+        text = "1 DEATH CULT EXECUTIONER"
+        indent = 1
+    ),
+      OperativeSelectionRule(
+        text = "1 LECTRO-MAESTER"
+        indent = 1
+    ),
+      OperativeSelectionRule(
+        text = "1 REJUVENAT ADEPT"
+        indent = 1
+    ),
+      OperativeSelectionRule(
+        text = "1 VOIDMASTER"
+        indent = 1
+    ),
+      OperativeSelectionRule(
+        text = "3 VOIDSMAN with Lasgun and Gun Butt"
+        indent = 1
+    ),
+      OperativeSelectionRule(
+        text = "1 VOIDSMAN with Rotor Cannon and Gun Butt"
+        indent = 1
+    ),
+
+)
