5522a40f708e808e318f6af18d4b918abd1ba108
Gtwo <131792434+MitolGtwo@users.noreply.github.com>
2026-02-19 19:34:20 -0500

    Create ElucidianOperatives.kt

---
+++ app/src/main/java/com/example/killteamruleset/ui/data/Empire/list/ElucidianStarstriders/ElucidianOperatives.kt
@@ -0,0 +1,15 @@
+package com.example.killteamruleset.ui.data.Empire.list.ElucidianStarstriders
+
+
+import com.example.killteamruleset.ui.model.Operative
+
+val ElucidianOperatives = listOf(
+EluciaVhan,
+Canid,
+DeathCultExecutioner,
+LectroMaester,
+RejuvenatAdept,
+VoidMaster,
+Voidsman
+
+)
